59b0e7b19f5f29ac2bad659c65515bca987b09e2
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-10 14:33:50 +0300

    Socket.io configurations

--- Socket_IO_2/server/socket/index.js
+++ Socket_IO_2/server/socket/index.js
@@ -1,3 +1,50 @@
 const Message = require('../models/Message');
-const User = require('../models/User');
-const Room = require('../models/Room');
+const User = require('../models/user');
+
+module.exports = (io) => {
+    io.on("connection", (socket) => {
+        console.log("Socket connected: ", socket.id)
+
+        socket.on("joinRoom", async (username, roomId) => {
+            const user = await User.findOneAndUpdate(
+                {username}, 
+                {socketId : socket.id, isOnline : true},
+                {new : true}
+            );
+
+            socket.join(roomId);
+            io.to(roomId).emit("userJoined", {user, roomId});
+
+            //Typing
+            socket.on("typing", () => {
+                io.to(roomId).emit("typing", username);
+            });
+
+            socket.on("stopTyping", () => {
+                io.to(roomId).emit("stopTyping", username);
+            });
+
+            //send message
+            socket.on("sendMessage", async (data) => {
+                const message = await Message.create({
+                    sender : user._id,
+                    room : roomId,
+                    content : data
+                });
+
+                const fullMessage = await Message.populate("sender", "username");
+                io.to(roomId).emit("newMessage", fullMessage);
+            });
+
+            //Disconnect
+            socket.on("disconnect", async() => {
+                const offlineUser = await User.findOneAndUpdate(
+                    {socketId: socket.id},
+                    {isOnline: false}
+                );
+                io.emit("userOffline", offlineUser.username);
+            });
+            
+        });
+    });
+};
