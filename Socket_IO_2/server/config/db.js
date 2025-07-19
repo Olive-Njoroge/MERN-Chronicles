f6bc85b0d649a2f43fa624a6efc02f88e5e2fc78
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-19 21:29:37 +0300

    update

--- Socket_IO_2/server/config/db.js
+++ Socket_IO_2/server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try{
-        await mongoose.connect(process.env.MONGO_URI_PRODUCTIO);
+        await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
         console.log("MongoDB connected!")
     }catch(error){
         console.error("Trouble connecting!", error.message);
